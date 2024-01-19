5411f5e9d2474a1ad924443e9d2fa3ea9c4a5ce7
Amarildo <amarildos@outloo.it>
2024-01-19 15:43:51 +0100

    added message on h1 by Vue js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 console.log('js ok');
 // collegamento Vue js
 console.log('Vue ok', Vue);
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello!'
+      }
+    }
+  }).mount('#root')
